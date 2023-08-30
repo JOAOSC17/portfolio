ece5544bfcb3a34c5fc10e50eda2450c1a41ec50
João Costa <costav1402@gmail.com>
2023-08-30 19:44:31 -0300

    feat: add profile pic

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,38 @@
-import { Box, Container, Heading } from "@chakra-ui/react"
+import { Box, Container, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
 const Page = () => {
-    return (
-       <Container>
-            <Box borderRadius={"lg"} bg="red" p={3} mb={6} align={'center'} >
-                Hello, I&apos;m a full-stack developer based in Brazil!
-            </Box>
-            <Box display={{md: 'flex'}} >
-                <Box flexGrow={1}>
-                    <Heading as="h2" variant="page-title">
-                        João Costa
-                    </Heading>
-                    {/* <p>( Developer / Designer )</p> */}
-                </Box>
-            </Box>
-       </Container>
-    )
+  return (
+    <Container>
+      <Box borderRadius={'lg'} bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} ali={'center'}>
+        Hello, I&apos;m a full-stack developer based in Brazil!
+      </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            João Costa
+          </Heading>
+          {/* <p>( Developer / Designer )</p> */}
+        </Box>
+        <Box 
+            flexShrink={0} 
+            mt={{ base: 4, md: 0 }} 
+            ml={{ md: 6 }} 
+            align="center"
+            >
+            <Image 
+            borderColor="whiteAlpha.800"
+            borderWidth={2} 
+            borderStyle="solid" 
+            maxWidth="100px" 
+            display="inline-block"
+            borderRadius="full"
+            src="https://avatars.githubusercontent.com/u/77072063?v=4"
+            alt="Profile Image" 
+            />
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
