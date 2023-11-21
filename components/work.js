a8bdeceaac9af702bb95e0cef39f3940960e1a52
João Costa <costav1402@gmail.com>
2023-11-21 19:38:43 -0300

    feat: add work page

---
+++ components/work.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Nextlink from 'next/link'
+import { Heading, Box, Image, Link, Badge } from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+export const Title = ({ children }) => (
+    <Box>
+        <Nextlink href="/works">
+            Work
+        </Nextlink>
+        <span>
+            &nbsp;
+            <ChevronRightIcon />
+            &nbsp;
+        </span>
+        <Heading display="inline-block" as="h3" fontSize={20} mb={4}>
+            {children}
+        </Heading>
+    </Box>
+)
+
+export const WorkImage = ({ src, alt }) => (
+    <Image borderRadius="lg" w="full" src={src} alt={alt} mb={4} />
+)
+
+export const Meta = ({ children }) => (
+    <Badge colorScheme='green' mr={2}>
+        {children}
+    </Badge>
+)
